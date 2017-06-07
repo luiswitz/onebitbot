47ed4d33582cae7fe1dcb49eb777ca48f94080df
Luis Henrique Witz <luiswitz@gmail.com>
2017-06-06 21:31:00 -0300

    Added faq type to faqs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607002018) do
+ActiveRecord::Schema.define(version: 20170607002855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170607002018) do
     t.string "question"
     t.string "answer"
     t.integer "company_id"
+    t.integer "faq_type"
   end
 
   create_table "hashtags", force: :cascade do |t|
